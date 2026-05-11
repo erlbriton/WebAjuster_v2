f270cd51d944e1d13f587ea704ee709cd3565679
erlbriton <telebite@yandex.ru>
2026-05-11 19:55:51 +0700

    Начинаю новый WebAjuster

---
+++ composeApp/src/webMain/kotlin/org/example/project/ParameterData.kt
@@ -0,0 +1,30 @@
+package org.example.project
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class ParameterData(
+    val code: String,                // Например, p11200
+    var idName: String,              // Обозначение
+    var description: String,         // Описание
+    var unit: String,                // Ед. Изм.
+
+    // Свойства из твоего окна "Свойства параметра"
+    var scale: String = "0,1",
+    var dependency: String = "",
+    var factor: String = "1",
+    var correction: String = "1",
+
+    // Значения для таблицы
+    initialHexBase: String = "0000",
+    initialPhysBase: String = "0.0",
+    initialHexCtrl: String = "0000",
+    initialPhysCtrl: String = "0.0"
+) {
+    // Делаем значения реактивными, чтобы таблица обновлялась при вводе
+    var hexBase by mutableStateOf(initialHexBase)
+    var physBase by mutableStateOf(initialPhysBase)
+    var hexCtrl by mutableStateOf(initialHexCtrl)
+    var physCtrl by mutableStateOf(initialPhysCtrl)
+}
