2ffbfd91550de486e6d6d9c3b6674c403d4c6356
erlbriton <telebite@yandex.ru>
2026-05-14 20:08:25 +0700

    Корректировка начального положения таблицы.

--- composeApp/src/webMain/kotlin/org/example/project/viewmodels/MainViewModel.kt
+++ composeApp/src/webMain/kotlin/org/example/project/viewmodels/MainViewModel.kt
@@ -14,8 +14,8 @@ class MainViewModel {
     val colWeights = mutableStateListOf<Float>(
         0.05f,   // 0: №
         0.15f,   // 1: Имя
-        0.30f,   // 2: Описание
-        0.05f,   // 3: Ед.изм
+        0.29f,   // 2: Описание
+        0.06f,   // 3: Ед.изм
         0.1125f, // 4: hex База
         0.1125f, // 5: Phys База
         0.1125f, // 6: hex Контр
@@ -94,26 +94,14 @@ class MainViewModel {
     }
 
     fun loadSampleData() {
+        // Очищаем текущий список, чтобы таблица стала пустой
         parameters.clear()
-        val sample = listOf(
-            ParameterData("p10000","IstStart",        "Пуск по току статора",           "A",   initialHexBase="0014", initialPhysBase="20",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10100","IstStop",         "Стоп по току статора",           "A",   initialHexBase="000A", initialPhysBase="10",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10200","IstExcEnable",    "Допустимый ток статора",         "A",   initialHexBase="0190", initialPhysBase="400",  initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10300","ExcEnableFreq",   "Минимальная частота скольж.",    "Hz",  initialHexBase="0019", initialPhysBase="25",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10400","HsFreq",          "Тяжёлый пуск: максимум",         "Hz",  initialHexBase="0032", initialPhysBase="50",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10500","HsTime",          "Время тяжёлого пуска",           "sec", initialHexBase="0014", initialPhysBase="20",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10600","IExcRef",         "Ток возбуждения в ручном режиме","A",   initialHexBase="00C8", initialPhysBase="200",  initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10700","IExcTst",         "Ток проверки цепи скольжения",   "A",   initialHexBase="0064", initialPhysBase="100",  initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10800","IExcForce",       "Ток форсировки возбуждения",     "A",   initialHexBase="012C", initialPhysBase="300",  initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p10900","StartForceTime",  "Время пусковой форсировки",      "sec", initialHexBase="0BB8", initialPhysBase="3000", initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11000","IstOffTime",      "Задержка перед отключением",     "sec", initialHexBase="0BB8", initialPhysBase="3000", initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11100","TestTime",        "Длительность тестирования",      "sec", initialHexBase="0BB8", initialPhysBase="3000", initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11200","FieldBlankTime",  "Длительность гашения поля",      "sec", initialHexBase="0BB8", initialPhysBase="3000", initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11300","NextStartTime",   "Время блокировки повторного пуска","sec",initialHexBase="0005", initialPhysBase="5",    initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11400","FSAsyncTime",     "Время асинхронного хода 1",      "sec", initialHexBase="001E", initialPhysBase="30",   initialHexCtrl="0000", initialPhysCtrl="0"),
-            ParameterData("p11500","QminAsyncTime",   "Время асинхронного хода 2",      "sec", initialHexBase="0023", initialPhysBase="35",   initialHexCtrl="0000", initialPhysCtrl="0"),
-        )
-        parameters.addAll(sample)
+
+        // В будущем здесь будет вызов:
+        // val realData = repository.getData()
+        // parameters.addAll(realData)
+
+        println("Таблица очищена и готова к загрузке реальных данных.")
     }
 }
 
